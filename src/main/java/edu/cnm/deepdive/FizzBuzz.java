cac608cc0a660afcefd8e07bf075135cc682a8c4
Mark <mvega37@cnm.edu>
2026-01-28 15:11:01 -0700

    Moved Main

--- src/main/java/edu/cnm/deepdive/FizzBuzz.java
+++ src/main/java/edu/cnm/deepdive/FizzBuzz.java
@@ -18,15 +18,4 @@ public class FizzBuzz {
     return result;
   }
 
-  public static void main(String[] args) {
-    int max = 100;
-    int parsedArg;
-    if (args.length > 0 && (parsedArg = Integer.parseInt(args[0])) > 0) {
-      max = parsedArg;
-    }
-    FizzBuzz fizzBuzz = new FizzBuzz();
-    for (int i = 0; i < max; i++) {
-      System.out.println(fizzBuzz.fizzBuzz(i + 1));
-    }
-  }
 }
